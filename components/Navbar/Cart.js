95b554025d1fbe8d3568cea256eb36099f552853
Trreffy <ricalisboa@gmail.com>
2022-03-31 23:22:50 -0300

    New Cart Interface Design

--- components/Navbar/Cart.js
+++ components/Navbar/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCart } from '../../Hooks/CartHook'
 import Shipping from '../ProductInfo/Shipping.js'
 
@@ -6,46 +6,57 @@ import styles from './Navbar.module.css'
 import classNames from 'classnames'
 
 import Close from '../../svg/Close'
+import Trash from '../../svg/Trash'
 
 const Cart = ({ cartVisibility, setCartVisibility }) => {
     const [cart, setCart] = useCart()
+    const [render, setRender] = useState(0) // a useState to force the component to render after removing a specific item from cart
 
     let totalPrice = 0
     // Calc of total price
     Object.values(cart).map((item) => totalPrice += item.amount * item.price)
 
-    console.log(cart);
 
     const cartInterfaceClass = () => classNames( [styles.CartInterface], { [styles.Closed]: !cartVisibility } )
 
   return (
     <div className={cartInterfaceClass()} onClick={() => {setCartVisibility(false)}}>
         <div className={styles.CartGUI} onClick={(e) => e.stopPropagation()}>
+
           <div className={styles.head}>
             <button className={styles.removeAll} onClick={() => setCart([])} >Remove All</button>
             <Close className={styles.CloseIcon} onClick={() => setCartVisibility(false)}/>
           </div>
+
             <div className={styles.ProductDisplay}>
               {Object.values(cart).map((item, index) => (
+
                 <div className={styles.CartItem} key={index}>
                   <div className={styles.ImageContainer}>
                     <img src={item.image} alt={item.title}/>
                   </div>
                   <div className={styles.CartItemInfo}>
                     <div className={styles.CartItemHead}>
-                      <h1>{item.title.substring(0, 90)}...</h1>
+                      <h1>{item.title.substring(0, 80)}...</h1>
                       <button onClick={() => { 
-                        let newObj = Object.values(cart).filter( (product) => product.id !== item.id )
-                        setCart({ newObj })
-                       }}>Remove Item</button>
+                        delete cart[item.id]
+                        setCart( cart )
+                        setRender(render + 1)
+                       }}
+                       ><Trash className={styles.trashIcon}/></button>
                     </div>
                     <h4>$ {item.price}</h4>
                     <h6>x{item.amount}</h6>
                   </div>
                 </div>
+
               ))}
+
             </div>
-            <p className={styles.TotalPrice}><strong>Total Price:</strong> $ {totalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</p>
+            <p className={styles.TotalPrice}>
+              <strong>Total Price: </strong> 
+               $ {totalPrice}</p> {/* .toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") */}
+                      {/* a regex to put a space every 3 numbers */}
         </div>
     </div>
   )
---
+++ svg/Trash.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const Trash = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" {...props}>
+    <path fill="none" d="M0 0h256v256H0z" />
+    <path d="M215.996 48H176v-8a24.027 24.027 0 0 0-24-24h-48a24.027 24.027 0 0 0-24 24v8H39.996a8 8 0 0 0 0 16h8v144a16.018 16.018 0 0 0 16 16h128a16.018 16.018 0 0 0 16-16V64h8a8 8 0 0 0 0-16ZM112 168a8 8 0 0 1-16 0v-64a8 8 0 0 1 16 0Zm48 0a8 8 0 0 1-16 0v-64a8 8 0 0 1 16 0Zm0-120H96v-8a8.01 8.01 0 0 1 8-8h48a8.01 8.01 0 0 1 8 8Z" />
+  </svg>
+)
+
+export default Trash
