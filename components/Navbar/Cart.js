4cae3208e201785aaeea96b954f55f44b9373053
Trreffy <ricalisboa@gmail.com>
2022-04-01 22:33:54 -0300

    Cart interface Done

--- components/Navbar/Cart.js
+++ components/Navbar/Cart.js
@@ -7,17 +7,19 @@ import classNames from 'classnames'
 
 import Close from '../../svg/Close'
 import Trash from '../../svg/Trash'
+import style from './Navbar.module.css'
 
 const Cart = ({ cartVisibility, setCartVisibility }) => {
     const [cart, setCart] = useCart()
     const [render, setRender] = useState(0) // a useState to force the component to render after removing a specific item from cart
+    const [cartPage, setCartPage] = useState(0)
 
     let totalPrice = 0
     // Calc of total price
     Object.values(cart).map((item) => totalPrice += item.amount * item.price)
 
 
-    const cartInterfaceClass = () => classNames( [styles.CartInterface], { [styles.Closed]: !cartVisibility } )
+    const cartInterfaceClass = () => classNames( [styles.CartInterface], { [styles.Closed]: !cartVisibility }, { [styles.payment]: cartPage === 1 } )
 
   return (
     <div className={cartInterfaceClass()} onClick={() => {setCartVisibility(false)}}>
@@ -51,12 +53,25 @@ const Cart = ({ cartVisibility, setCartVisibility }) => {
                 </div>
 
               ))}
-
             </div>
-            <p className={styles.TotalPrice}>
-              <strong>Total Price: </strong> 
-               $ {totalPrice}</p> {/* .toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") */}
-                      {/* a regex to put a space every 3 numbers */}
+
+            { cartPage === 1 &&
+            <div className={styles.checkOut}>
+              <h1>Check Out</h1>
+
+              <h5>Please Select your deliver options</h5>
+            </div>
+            }
+            { cartPage === 1 && <Shipping /> }
+
+            <footer className={styles.CartFooter}>
+              <p className={styles.TotalPrice}>
+                <strong>Total Price: </strong> $ {totalPrice}
+              </p> {/* .toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") */}
+                        {/* a regex to put a space every 3 numbers */}
+              { cartPage === 0 && <button className={styles.NextButton} onClick={() => setCartPage(1)}> next </button> }
+              { cartPage === 1 && <button className={styles.PrevButton} onClick={() => setCartPage(0)}> prev </button>}
+            </footer>
         </div>
     </div>
   )
